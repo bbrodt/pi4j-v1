c031188eeb4f2bdadf97d0ed03af04fbfdc56634
Robert Brodt <rbrodt@gmail.com>
2021-12-01 08:36:01 -0700

    Merged release/1.3 component/device packages into release/1.4
    I needed the components & devices support as well as Java 11 for my WS4Pi project.
    The pom files have been updated to correctly build under Java 11.

---
+++ pi4j-device/src/test/java/com/pi4j/component/servo/impl/RPIServoBlasterProviderTest.java
@@ -0,0 +1,103 @@
+package com.pi4j.component.servo.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.pi4j.io.gpio.Pin;
+import com.pi4j.io.gpio.RaspiPin;
+
+/*
+ * #%L
+ * **********************************************************************
+ * ORGANIZATION  :  Pi4J
+ * PROJECT       :  Pi4J :: Device Abstractions
+ * FILENAME      :  RPIServoBlasterProviderTest.java
+ *
+ * This file is part of the Pi4J project. More information about
+ * this project can be found here:  https://pi4j.com/
+ * **********************************************************************
+ * %%
+ * Copyright (C) 2012 - 2021 Pi4J
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+/*
+ * Class:     RPIServoBlasterProviderTest
+ * Created:   5th February 2017
+ *
+ * @author Sam Hough
+ * @version 1.1, 5th February 2017
+ */
+public class RPIServoBlasterProviderTest {
+    private String resolvePhysical(Pin pin) {
+        return RPIServoBlasterProvider.PIN_MAP.get(pin);
+    }
+
+    @Test
+    public void wiringPi30Mapping() {
+        assertEquals("P1-27", resolvePhysical(RaspiPin.GPIO_30));
+    }
+
+    @Test
+    public void wiringPi31Mapping() {
+        assertEquals("P1-28", resolvePhysical(RaspiPin.GPIO_31));
+    }
+
+    @Test
+    public void wiringPi21Mapping() {
+        assertEquals("P1-29", resolvePhysical(RaspiPin.GPIO_21));
+    }
+
+    @Test
+    public void wiringPi22Mapping() {
+        assertEquals("P1-31", resolvePhysical(RaspiPin.GPIO_22));
+    }
+
+    @Test
+    public void wiringPi26Mapping() {
+        assertEquals("P1-32", resolvePhysical(RaspiPin.GPIO_26));
+    }
+
+    @Test
+    public void wiringPi23Mapping() {
+        assertEquals("P1-33", resolvePhysical(RaspiPin.GPIO_23));
+    }
+
+    @Test
+    public void wiringPi24Mapping() {
+        assertEquals("P1-35", resolvePhysical(RaspiPin.GPIO_24));
+    }
+
+    @Test
+    public void wiringPi27Mapping() {
+        assertEquals("P1-36", resolvePhysical(RaspiPin.GPIO_27));
+    }
+
+    @Test
+    public void wiringPi25Mapping() {
+        assertEquals("P1-37", resolvePhysical(RaspiPin.GPIO_25));
+    }
+
+    @Test
+    public void wiringPi28Mapping() {
+        assertEquals("P1-38", resolvePhysical(RaspiPin.GPIO_28));
+    }
+
+    @Test
+    public void wiringPi29Mapping() {
+        assertEquals("P1-40", resolvePhysical(RaspiPin.GPIO_29));
+    }
+}
---
+++ pi4j-gpio-extension/src/main/java/com/pi4j/gpio/extension/mcp/MCP3208GpioProvider.java
@@ -0,0 +1,152 @@
+package com.pi4j.gpio.extension.mcp;
+
+/*
+ * #%L
+ * **********************************************************************
+ * ORGANIZATION  :  Pi4J
+ * PROJECT       :  Pi4J :: GPIO Extension
+ * FILENAME      :  MCP3208GpioProvider.java
+ *
+ * This file is part of the Pi4J project. More information about
+ * this project can be found here:  https://pi4j.com/
+ * **********************************************************************
+ * %%
+ * Copyright (C) 2012 - 2021 Pi4J
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.io.IOException;
+
+import com.pi4j.gpio.extension.base.AdcGpioProvider;
+import com.pi4j.io.spi.SpiChannel;
+import com.pi4j.io.spi.SpiDevice;
+import com.pi4j.io.spi.SpiMode;
+
+/**
+ *
+ * <p>
+ * This GPIO provider implements the MCP3208 SPI GPIO expansion board as native Pi4J GPIO pins. It is a 12-bit ADC
+ * providing 8 input channels.
+ * </p>
+ *
+ * <p>
+ * The MCP3008 is connected via SPI connection to the Raspberry Pi and provides 8 GPIO pins that can be used for analog
+ * input pins. The values returned are in the range 0-4095 (10 bit value).
+ *
+ * Note: This implementation currently only supports single-ended inputs.
+ * </p>
+ *
+ * @author pojd, Hendrik Motza
+ */
+public class MCP3208GpioProvider extends MCP3x0xGpioProvider implements AdcGpioProvider {
+
+	public static final String NAME = "com.pi4j.gpio.extension.mcp.MCP3208GpioProvider";
+	public static final String DESCRIPTION = "MCP3208 GPIO Provider";
+	public static final int INPUT_COUNT = 8;
+	public static final int RESOLUTION = 12;
+
+	/**
+	 * Create new instance of this MCP3208 provider with background monitoring and pin notification events enabled.
+	 *
+	 * @param channel
+	 *            spi channel the MCP3208 is connected to
+	 * @throws IOException
+	 *             if an error occurs during initialization of the SpiDevice
+	 */
+	public MCP3208GpioProvider(final SpiChannel channel) throws IOException {
+		this(channel, SpiDevice.DEFAULT_SPI_SPEED, SpiDevice.DEFAULT_SPI_MODE, true);
+	}
+
+	/**
+	 * Create new instance of this MCP3208 provider with background monitoring and pin notification events enabled.
+	 *
+	 * @param channel
+	 *            spi channel the MCP3208 is connected to
+	 * @param speed
+	 *            spi speed to communicate with MCP3208
+	 * @throws IOException
+	 *             if an error occurs during initialization of the SpiDevice
+	 */
+	public MCP3208GpioProvider(final SpiChannel channel, final int speed) throws IOException {
+		this(channel, speed, SpiDevice.DEFAULT_SPI_MODE, true);
+	}
+
+	/**
+	 * Create new instance of this MCP3208 provider with background monitoring and pin notification events enabled.
+	 *
+	 * @param channel
+	 *            spi channel the MCP3208 is connected to
+	 * @param mode
+	 *            spi mode to communicate with MCP3208
+	 * @throws IOException
+	 *             if an error occurs during initialization of the SpiDevice
+	 */
+	public MCP3208GpioProvider(final SpiChannel channel, final SpiMode mode) throws IOException {
+		this(channel, SpiDevice.DEFAULT_SPI_SPEED, mode, true);
+	}
+
+	/**
+	 * Create new instance of this MCP3208 provider with background monitoring and pin notification events enabled.
+	 *
+	 * @param channel
+	 *            spi channel the MCP3208 is connected to
+	 * @param speed
+	 *            spi speed to communicate with MCP3208
+	 * @param mode
+	 *            spi mode to communicate with MCP3208
+	 * @throws IOException
+	 *             if an error occurs during initialization of the SpiDevice
+	 */
+	public MCP3208GpioProvider(final SpiChannel channel, final int speed, final SpiMode mode) throws IOException {
+		this(channel, speed, mode, true);
+	}
+
+	/**
+	 * Create new instance of this MCP3208 provider. Optionally enable or disable background monitoring and pin
+	 * notification events.
+	 *
+	 * @param channel
+	 *            spi channel the MCP3208 is connected to
+	 * @param speed
+	 *            spi speed to communicate with MCP3208
+	 * @param mode
+	 *            spi mode to communicate with MCP3208
+	 * @param enableBackgroundMonitoring
+	 *            if enabled, then a background thread will be created to constantly acquire the ADC input values and
+	 *            publish pin change listeners if the value change is beyond the configured threshold.
+	 * @throws IOException
+	 *             if an error occurs during initialization of the SpiDevice
+	 */
+	public MCP3208GpioProvider(final SpiChannel channel, final int speed, final SpiMode mode,
+			final boolean enableBackgroundMonitoring) throws IOException {
+		super(MCP3208Pin.ALL, channel, speed, RESOLUTION, mode);
+
+		// default background monitoring interval
+		setMonitorInterval(DEFAULT_MONITOR_INTERVAL);
+
+		// enable|disable background monitoring
+		if (enableBackgroundMonitoring) {
+			setMonitorEnabled(enableBackgroundMonitoring);
+		}
+	}
+
+	// ------------------------------------------------------------------------------------------
+	// PUBLIC METHODS
+	// ------------------------------------------------------------------------------------------
+	@Override
+	public String getName() {
+		return NAME;
+	}
+}
